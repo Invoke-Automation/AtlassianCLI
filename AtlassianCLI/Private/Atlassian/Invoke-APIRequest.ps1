14e3cc6655deefa3d0e19f09c13da5c2b41a10a1
Tomas Deceuninck <tomas.deceuninck@gmail.com>
2017-02-22 00:46:55 +0100

    minor rework on Invoke-APIRequest

--- AtlassianCLI/Private/Atlassian/Invoke-APIRequest.ps1
+++ AtlassianCLI/Private/Atlassian/Invoke-APIRequest.ps1
@@ -41,11 +41,13 @@ function Invoke-APIRequest {
 			}
 			$Uri = ('{0}{1}' -f $Session.Server,$Uri)
 		}
+		Write-Debug -Message ('Method:{0} Uri:{1}' -f $method,$uri)
 		$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $Session.Credential.UserName,([Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($Session.Credential.Password))))))
+		$headers = @{Authorization=("Basic {0}" -f $base64AuthInfo)}
 		if($Body){
-			Invoke-RestMethod -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} -Method $Method -Uri $Uri -Body $Body
+			Invoke-RestMethod -Headers $headers -Method $Method -Uri $Uri -Body $Body -ContentType 'application/json'
 		} else {
-			Invoke-RestMethod -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} -Method $Method -Uri $Uri
+			Invoke-RestMethod -Headers $headers -Method $Method -Uri $Uri
 		}
 	}
 	End{}
